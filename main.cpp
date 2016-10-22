7df289eebc9d756454ab9bc732b6aa0426ff8657
brettmccausland <brettmccausland@yahoo.com>
2016-10-22 13:06:02 -0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-
-
+void Exercise13_2();
+void Exercise13_3();
+void Exercise13_4();
+void Exercise13_5();
 
 int main()
 {
@@ -12,4 +13,35 @@ int main()
 
 
   return 0;
+}
+void Exercise13_2()
+{
+  //Write functions
+  //set<int> set_union(set<int> a, set<int> b)
+  //set<int> intersection(set<int> a, set<int> b)
+  //that compute the set union
+  //and intersection of the sets a and b.
+  //(Don’t name the first function union that is a reserved word in C++.)
+}
+void Exercise13_3()
+{
+  // Implement the sieve of Eratosthenes: a function
+  // for computing prime numbers,
+  // known to the ancient Greeks.
+  // Choose an integer n.
+  // This function will compute all prime numbers up to n.
+  // First insert all numbers from 1 to n into a set.
+  // Then erase all multiples of 2 (except 2); that is, 4, 6, 8, 10, 12, ....
+  // Erase all multiples of 3, that is, 6, 9, 12, 15, ... .
+  // Go up to ￼ n . The remaining numbers are all primes.
+
+}
+void Exercise13_4()
+{
+  //Write a program that counts how often each word occurs in a text file.
+  //Use a multiset<string>.
+}
+void Exercise13_5()
+{
+  //Repeat Exercise P13.4, but use a map<string, int>.
 }
